8ed7843f02e82c2843418c719d273b331e0daac0
abhip <apardhi20@iitk.ac.in>
2022-04-09 15:30:01 +0530

    6th part done

--- shell.sh
+++ shell.sh
@@ -18,9 +18,10 @@ echo "|-------------------------------------------------------------------------
 # print $1 }' >> Output.txt
 # awk -F',' '{if($3~"Bachelor") print $1;}' Input.csv >> Output.txt
 
-awk -F',' '$3 == "Bachelor\x27s" { print $1 }' Input.csv >> Output.txt
+awk -F',' '$3 == "Bachelor\x27s" { printf("| %s\n",$1) }' Input.csv >> Output.txt
+
 echo "|-------------------------------------------------------------------------------------------------------------------------" >> Output.txt
-echo "| Geography: Average Admission Rate" >> Output.txt
+echo "| Geography       : Average Admission Rate " >> Output.txt
 echo "|-------------------------------------------------------------------------------------------------------------------------" >> Output.txt
 
 # awk -F',' '{
@@ -52,22 +53,49 @@ echo "|-------------------------------------------------------------------------
 # }'
 # Input.csv >> Output.txt
 
-awk -F',' 'BEGIN {
-    print NR
-    for (i = 1; i <= NR; i++)
-    {
-        print $0
-    }
-}' Input.csv >> Output.txt
+# awk -F',' 'BEGIN {
+#     print NR
+#     for (i = 1; i <= NR; i++)
+#     {
+#         print $0
+#     }
+# }' Input.csv >> Output.txt
 # awk -F',' '{print NR-1 "," $0}' Input.csv >> Output.txt
 
 # awk -F',' 'BEGIN{ for (i=1; i<=2; i++)
 # for (j=1; j<=3; j++)
 # print "haha";}' Input.csv >> Output.txt
-awk -F',' '{ arr[$6] += $7 ; brr[$6] ++}
+awk -F',' '{ arr[$6] += $7 ; brr[$6] ++ ; if(max<length($6)) max=length($6)}
 END {
 for (var in arr)
-    printf("%s: %0.4f\n",var,arr[var]/brr[var])
+{
+    printf("| %s",var)
+    space=max-length(var)+1
+    for(i=0;i<space;i++) printf " ";
+    printf(": %0.4f\n",arr[var]/brr[var])
+}
 }' Input.csv >> Output.txt
 
 # awk -F',' '{ sum += $7 } END { if (NR > 0) printf("%0.4f", sum/NR) }' Input.csv >> Output.txt
+
+echo "|-------------------------------------------------------------------------------------------------------------------------" >> Output.txt
+echo "| Top 5 colleges having maximum MedianEarnings   : MedianEarning" >> Output.txt
+echo "|-------------------------------------------------------------------------------------------------------------------------" >> Output.txt
+
+awk -F',' '{ crr[$1] += $16 }
+END {
+PROCINFO["sorted_in"] = "@val_num_desc"
+k=0
+for ( i in crr )
+{
+    if(k==5) break;
+    printf("| %s",i)
+    space=47-length(i)
+    for(j=0;j<space;j++) printf " ";
+    print ":", crr[i]
+    k++
+}
+}' Input.csv >> Output.txt
+echo "|-------------------------------------------------------------------------------------------------------------------------" >> Output.txt
+
+# @val_num_asc
